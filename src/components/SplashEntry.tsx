2c15396b258e9efc418dc0576b6878b47c5c018d
Brian Smiley <briantsmiley42@gmail.com>
2024-10-22 16:57:59 -0400

    adds lock to password input

--- src/components/SplashEntry.tsx
+++ src/components/SplashEntry.tsx
@@ -3,13 +3,15 @@
 import { useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { Lock } from "lucide-react";
 
 export default function SplashEntry() {
   const [showPasswordInput, setShowPasswordInput] = useState(false);
   return (
     <div className="h-24 flex flex-col items-center justify-center">
       {showPasswordInput ? (
-        <div>
+        <div className="relative">
+          <Lock className="absolute text-gray-500 top-1/2 -translate-y-1/2 right-1" />
           <Input
             type="password"
             className="w-48 h-12 text-2xl"
