5f32ab87561e7137622e1bee75287dcbe197bd13
Brian Smiley <briantsmiley42@gmail.com>
2024-10-22 17:03:47 -0400

    restyling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <div className="flex flex-col items-center justify-center gap-10 h-full bg-slate-950 bg-opacity-70 p-12">
-        <h1 className="satisfy text-8xl text-white">Thirst Trap</h1>
+      <Image
+        src="/ThirstTrapCover.png"
+        alt="Thirst Trap"
+        width={920}
+        height={768}
+        className="absolute m-auto -z-10 opacity-70"
+      />
+      <div className="flex flex-col items-center justify-center gap-10 h-full w-[70%] bg-slate-950 bg-opacity-50 p-12">
+        <h1 className="satisfy text-8xl text-white text-center">Thirst Trap</h1>
         <SplashEntry />
-        <Image
-          src="/ThirstTrapCover.png"
-          alt="Thirst Trap"
-          width={920}
-          height={768}
-          className="absolute inset-0 m-auto -z-10 opacity-50"
-        />
       </div>
     </div>
   );
