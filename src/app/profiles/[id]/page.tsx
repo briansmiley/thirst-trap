42dec51c45fe7dd95c8ae9d5e16e3230c5e8c4fc
Rebecca Peckman <rebeccapeckman@gmail.com>
2024-10-22 23:17:19 -0400

    profile/[id] page now displays player data or form for new player

--- src/app/profiles/[id]/page.tsx
+++ src/app/profiles/[id]/page.tsx
@@ -1,4 +1,12 @@
+import playerService from "@/server/services/player";
+
 import NewProfileForm from "./new-profile-form";
+import PlayerInfo from "./player-info";
+
+async function getPlayerData(id: string) {
+  "use server";
+  return await playerService.get(id);
+}
 
 export default async function ProfilePage({
   params,
@@ -6,6 +14,11 @@ export default async function ProfilePage({
   params: { id: string };
 }) {
   const { id } = await params;
+  const playerData = await getPlayerData(id);
 
-  return <NewProfileForm id={id} />;
+  return playerData ? (
+    <PlayerInfo {...playerData} />
+  ) : (
+    <NewProfileForm id={id} />
+  );
 }
