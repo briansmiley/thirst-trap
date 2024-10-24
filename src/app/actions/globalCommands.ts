4a5b7934fd604132595aa0149e575bdf1292fc1f
Brian Smiley <briantsmiley42@gmail.com>
2024-10-24 11:28:51 -0400

    adds commands page for global pause/unpause

---
+++ src/app/actions/globalCommands.ts
@@ -0,0 +1,9 @@
+"use server";
+
+export async function pauseAll() {
+  console.log("Pausing all from server action");
+}
+
+export async function unpauseAll() {
+  console.log("Unpausing all from server action");
+}
--- src/app/commands/page.tsx
+++ src/app/commands/page.tsx
@@ -1,11 +1,32 @@
 import { Button } from "@/components/ui/button";
+import { Pause, Play } from "lucide-react";
+import { pauseAll, unpauseAll } from "@/app/actions/globalCommands";
 
 export default function Commands() {
   return (
-    <div>
-      <div>
-        <span>Pause All Timers</span>
-        <Button></Button>
+    <div className="flex flex-col items-center w-full h-full justify-center text-2xl">
+      <div className="flex w-96 flex-row items-center justify-between gap-2">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <span>Pause All</span>
+          <form action={pauseAll}>
+            <Button variant="outline" className="w-24 h-20" type="submit">
+              <Pause className="!size-12" />
+            </Button>
+          </form>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-2">
+          <span>Unpause All</span>
+          <form action={unpauseAll}>
+            <Button
+              variant="outline"
+              size="icon"
+              className="flex p-0 w-24 h-20"
+              type="submit"
+            >
+              <Play className="!size-12" />
+            </Button>
+          </form>
+        </div>
       </div>
     </div>
   );
