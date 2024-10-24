1d6f168a0fe89b65f7d21d268fd23a40a2954c68
Brian Smiley <briantsmiley42@gmail.com>
2024-10-24 11:47:48 -0400

    adds logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="bg-neutral-900 text-neutral-50 h-16 flex items-center">
       <ul className="flex h-full">
-        <li className="">
+        <li className="flex items-center justify-center px-4">
+          <Image alt="logo" src="/favicon.svg" width={24} height={24} />
+        </li>
+        <li>
           <Link
             href="/dashboard"
             className="hover:text-neutral-300 hover:bg-neutral-800 h-full flex justify-center items-center px-4"
@@ -12,7 +16,7 @@ const Navbar = () => {
             Dashboard
           </Link>
         </li>
-        <li className="">
+        <li>
           <Link
             href="/commands"
             className="hover:text-neutral-300 hover:bg-neutral-800 h-full flex justify-center items-center px-4"
@@ -20,7 +24,7 @@ const Navbar = () => {
             Commands
           </Link>
         </li>
-        <li className="">
+        <li>
           <Link
             href="/settings"
             className="hover:text-neutral-300 hover:bg-neutral-800 h-full flex justify-center items-center px-4"
