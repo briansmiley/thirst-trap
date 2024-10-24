bcefeb3d8d1d8abd3d1af29637841a250e2c87c8
Rebecca Peckman <rebeccapeckman@gmail.com>
2024-10-24 15:55:31 -0400

    added updatePlayer websocket event, loggable util function for truncating player picture string in server logs

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,10 +2,8 @@ import { createServer } from "node:http";
 import next from "next";
 import { Server } from "socket.io";
 import playerService from "./services/player";
-import {
-  ClientToServerEvents,
-  ServerToClientEvents,
-} from "./interface";
+import { ClientToServerEvents, ServerToClientEvents } from "./interface";
+import { loggable } from "./utils";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = process.env.HOSTNAME || "localhost";
@@ -17,10 +15,7 @@ const handler = app.getRequestHandler();
 app.prepare().then(() => {
   const httpServer = createServer(handler);
 
-  const io = new Server<
-    ClientToServerEvents,
-    ServerToClientEvents
-  >(
+  const io = new Server<ClientToServerEvents, ServerToClientEvents>(
     httpServer
     //   , {
     //   cors: {
@@ -34,12 +29,28 @@ app.prepare().then(() => {
     console.log("ON connected:", socket.id);
 
     socket.on("addPlayer", (player, callback) => {
-      console.log("ON addPlayer:", socket.id, {...player, picture: player.picture.slice(0, 50)});
+      console.log("ON addPlayer:", socket.id, loggable(player));
       playerService
         .create(player)
         .then((newPlayer) => {
-          console.log("EMIT addPlayer:", newPlayer);
+          console.log("EMIT addPlayer:", loggable(newPlayer));
           io.emit("addPlayer", newPlayer);
+          callback({ success: true });
+        })
+        .catch((err) => {
+          console.error(err);
+          callback({ success: false, message: err.message });
+        });
+    });
+
+    socket.on("updatePlayer", (player, callback) => {
+      console.log("ON updatePlayer:", socket.id, loggable(player));
+      const { playerId, ...rest } = player;
+      playerService
+        .update(playerId, rest)
+        .then((updatedPlayer) => {
+          console.log("EMIT updatePlayer:", loggable(updatedPlayer));
+          io.emit("updatePlayer", updatedPlayer);
           callback({ success: true });
         })
         .catch((err) => {
---
+++ src/server/utils.ts
@@ -0,0 +1,8 @@
+import { Player } from "@/app/types";
+
+export function loggable(player: Partial<Player>) {
+  return {
+    ...player,
+    ...(player.picture ? { picture: player.picture.slice(0, 50) } : {}),
+  };
+}
