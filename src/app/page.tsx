f8823ea3b2bd8e614ee53d8eb6e37094c1b2d2dd
Brian Smiley <briantsmiley42@gmail.com>
2024-10-24 15:19:57 -0400

    makes background an img instead of Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import SplashEntry from "@/components/SplashEntry";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <Image
+      <img
         src="/ThirstTrapCover.png"
         alt="Thirst Trap"
         width={920}
