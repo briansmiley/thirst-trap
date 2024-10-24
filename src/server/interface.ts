13ac3820e29e95884b024a5932362ca5c591f41f
Rebecca Peckman <rebeccapeckman@gmail.com>
2024-10-24 15:55:31 -0400

    added updatePlayer websocket event, loggable util function for truncating player picture string in server logs

--- src/server/interface.ts
+++ src/server/interface.ts
@@ -2,11 +2,16 @@ import { Player } from "@/app/types";
 
 export interface ServerToClientEvents {
   addPlayer: (player: Player) => void;
+  updatePlayer: (player: Partial<Player> & Pick<Player, "playerId">) => void;
 }
 
 export interface ClientToServerEvents {
   addPlayer: (
     player: Pick<Player, "name" | "playerId" | "picture">,
+    callback: (response: { success: boolean; message?: string }) => void
+  ) => void;
+  updatePlayer: (
+    player: Partial<Player> & Pick<Player, "playerId">,
     callback: (response: { success: boolean; message?: string }) => void
   ) => void;
 }
