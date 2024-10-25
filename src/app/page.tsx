9997c1306c62a1f349c8339cbf85482bcbb7d043
Brian Smiley <briantsmiley42@gmail.com>
2024-10-25 01:50:00 -0400

    !

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import SplashEntry from "@/components/SplashEntry";
+import SplashEntry from '@/components/SplashEntry'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
+    <div className="flex h-full flex-col items-center justify-center">
       <img
         src="/images/ThirstTrapCover.png"
         alt="Thirst Trap"
         width={920}
         height={768}
-        className="absolute m-auto -z-10 opacity-70"
+        className="absolute -z-10 m-auto opacity-70"
       />
-      <div className="flex flex-col items-center justify-center gap-10 h-full w-[70%] bg-slate-950 bg-opacity-70 p-12">
-        <h1 className="satisfy text-8xl text-white text-center">Thirst Trap</h1>
+      <div className="flex h-full w-[70%] flex-col items-center justify-center gap-10 bg-slate-950 bg-opacity-70 p-12">
+        <h1 className="satisfy text-center text-8xl text-white">Thirst Trap</h1>
         <SplashEntry />
-        <p className="bg-slate-500 bg-opacity-50 rounded p-2 font-mono">
-          This page is for party facilitators only; please enjoy the game IRL.
+        <p className="rounded bg-slate-500 bg-opacity-50 p-2 font-mono">
+          This page is for party facilitators only; please enjoy the game IRL!
         </p>
       </div>
     </div>
-  );
+  )
 }
