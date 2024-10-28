e68468bc13f281ed5d11b1c31c2a01ab75711370
Brian Smiley <briantsmiley42@gmail.com>
2024-10-28 19:23:10 -0400

    test push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <h1 className="satisfy text-center text-8xl text-white">Thirst Trap</h1>
         <SplashEntry />
         <p className="rounded bg-slate-500 bg-opacity-50 p-2 font-mono">
-          This page is for party facilitators only; please enjoy the game IRL!
+          This page is for party facilitators only; please enjoy the game IRL.
         </p>
       </div>
     </div>
