85b8814fd2e3b1134acec4f21c97cba1f3442b6f
Rebecca Peckman <rebeccapeckman@gmail.com>
2024-10-28 17:27:51 -0400

    hooked up profile page to store and simplified file structure

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,45 @@
-import { useSocketSubscription } from '@/socket/client'
-import PlayerProfileFinder from './PlayerProfileFinder'
-import { Player } from '../types'
+'use client'
+
 import { useState } from 'react'
-import playerService from '@/server/services/player'
+import { useRouter } from 'next/navigation'
+import { Player } from '@/app/types'
+import { useAppStore } from '@/lib/stores/AppStoreProvider'
+import { Button } from '@/components/ui/button'
+import { PlayersDropdown } from '@/app/profile/PlayersDropdown'
 
-const fetchPlayerIds = async () => {
-  const playerIds = await playerService.getAll(['playerId'])
-  const sortedPlayerIds = playerIds.sort()
-  return sortedPlayerIds
-}
+export default function ProfilePage() {
+  const playerIds = useAppStore((state) => state.players.map((p) => p.playerId).sort())
+  const [currentPlayerId, setCurrentPlayerId] = useState<Player['playerId']>()
+  const router = useRouter()
 
-export default async function ProfilePage() {
-  const playerIds = (await fetchPlayerIds()).map((player) => player.playerId)
-  return <PlayerProfileFinder playerIds={playerIds} />
+  const handleSubmit = () => {
+    if (!currentPlayerId) return
+    router.push(`/profile/${currentPlayerId}`)
+  }
+
+  const handleSelect = (playerId: string) => {
+    setCurrentPlayerId(playerId)
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="my_box flex flex-col items-center justify-center gap-4 p-12">
+        <h1 className="satisfy text-4xl font-bold">Player Profiles</h1>
+        <div className="flex flex-col items-center justify-center gap-4">
+          {/* <input
+            type="text"
+            className="rounded border-2 border-neutral-700 px-1"
+            name="id"
+            placeholder="Player Badge ID"
+          /> */}
+          <PlayersDropdown
+            playerIds={playerIds}
+            onSelect={handleSelect}
+            currentSelection={currentPlayerId ?? ''}
+          />
+          <Button onClick={handleSubmit}>Go to Profile</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
